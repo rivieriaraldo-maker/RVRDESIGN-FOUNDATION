f2693efb69eb101c910bfad31599f08ed342e38e
rivieriaraldo-maker <rivieriaraldo@gmail.com>
2026-07-10 17:39:10 -0300

    Add exports for IKEA-related modules

--- rvrdesign/workspace/index.js
+++ rvrdesign/workspace/index.js
@@ -10,3 +10,9 @@ export { default as RVRWorkspaceSlot } from './RVRWorkspaceSlot.jsx';
 export { RVR_WORKSPACE_REGISTRY, getRegistryEntry, isRegistered } from './RVRWorkspaceRegistry.js';
 export { RVR_WORKSPACE_PRESETS, getPreset } from './RVRWorkspacePresets.js';
 export { CONTRACT_STUBS } from './RVRWorkspaceContracts.js';
+
+export { clampSpan, getColumnSpanStyle, computeGridPosition } from './ikea/RVRGridEngine.js';
+export { snapToGrid, nearestColumnSpan } from './ikea/RVRSnapSystem.js';
+export { computeLayout } from './ikea/RVRLayoutEngine.js';
+export { validateSlotAssignment } from './ikea/RVRComponentSlots.js';
+export { useRVRSelectionLayer } from './ikea/RVRSelectionLayer.js';
